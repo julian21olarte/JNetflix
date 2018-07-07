7668857ca7097f49bc54310da484d163009e7dd6
julian21olarte <julian.olarte@rokk3rlabs.com>
2018-07-07 14:31:41 -0500

    Add cors for frontend requests.

--- src/main/java/com/julianolarte/netflix/NetflixApplication.java
+++ src/main/java/com/julianolarte/netflix/NetflixApplication.java
@@ -4,11 +4,27 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class NetflixApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NetflixApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE")
+                .allowCredentials(true)
+                .maxAge(60 * 60 * 24);
+            }
+        };
     }
 }
